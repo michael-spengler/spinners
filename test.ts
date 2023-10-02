3aa8807ac81af3806b53a33aa3dbcd8b1fc81d2e
Benjamin Pollack <bpollack@theknotww.com>
2023-10-02 12:09:06 -0400

    update to contemporary formatting and dependencies

--- test.ts
+++ test.ts
@@ -4,88 +4,89 @@ import { expect } from "https://deno.land/x/expect/mod.ts";
 import { sleep } from "https://x.nest.land/sleep@1.0.0/mod.ts";
 
 class TestWriter implements Deno.WriterSync {
-	buffer: string[] = [];
-	writeSync(p: Uint8Array): number {
-		this.buffer.push(new TextDecoder().decode(p));
-		return p.length;
-	}
+  buffer: string[] = [];
+  writeSync(p: Uint8Array): number {
+    this.buffer.push(new TextDecoder().decode(p));
+    return p.length;
+  }
 }
 
 Deno.test("spinner isSpinning when running", () => {
-	const terminalSpinner = new TerminalSpinner();
-	terminalSpinner.start();
-	expect(terminalSpinner.isSpinning()).toEqual(true);
-	terminalSpinner.stop();
+  const terminalSpinner = new TerminalSpinner();
+  terminalSpinner.start();
+  expect(terminalSpinner.isSpinning()).toEqual(true);
+  terminalSpinner.stop();
 });
 
 Deno.test("spinner !isSpinning when not running", () => {
-	const terminalSpinner = new TerminalSpinner().start();
-	terminalSpinner.stop();
-	expect(terminalSpinner.isSpinning()).toEqual(false);
+  const terminalSpinner = new TerminalSpinner().start();
+  terminalSpinner.stop();
+  expect(terminalSpinner.isSpinning()).toEqual(false);
 });
 
 Deno.test("stopAndPersist stops the spinner output", async () => {
-	const testWriter = new TestWriter();
-	const terminalSpinner = new TerminalSpinner({ text: "", writer: testWriter }).start();
-	terminalSpinner?.stopAndPersist();
+  const testWriter = new TestWriter();
+  const terminalSpinner = new TerminalSpinner({ text: "", writer: testWriter })
+    .start();
+  terminalSpinner.stopAndPersist();
 
-	// Wait and check that there are no extra prints
-	const sizeAfterStop = testWriter.buffer.length;
-	await sleep(1000);
-	expect(terminalSpinner?.isSpinning()).toEqual(false);
-	expect(sizeAfterStop).toEqual(testWriter.buffer.length);
+  // Wait and check that there are no extra prints
+  const sizeAfterStop = testWriter.buffer.length;
+  await sleep(1000);
+  expect(terminalSpinner?.isSpinning()).toEqual(false);
+  expect(sizeAfterStop).toEqual(testWriter.buffer.length);
 });
 
 Deno.test("renderNextFrame() advances the spinner", () => {
-	const testWriter = new TestWriter();
-	const terminalSpinner = new TerminalSpinner({
-		text: "",
-		writer: testWriter,
-	}).start();
-	terminalSpinner.stopAndPersist();
+  const testWriter = new TestWriter();
+  const terminalSpinner = new TerminalSpinner({
+    text: "",
+    writer: testWriter,
+  }).start();
+  terminalSpinner.stopAndPersist();
 
-	const sizeAfterStop = testWriter.buffer.length;
-	terminalSpinner.renderNextFrame();
+  const sizeAfterStop = testWriter.buffer.length;
+  terminalSpinner.renderNextFrame();
 
-	// Check that the frame is advancing
-	const sizeAfterNextStop = testWriter.buffer.length;
-	expect(sizeAfterStop).toBeLessThan(sizeAfterNextStop);
+  // Check that the frame is advancing
+  const sizeAfterNextStop = testWriter.buffer.length;
+  expect(sizeAfterStop).toBeLessThan(sizeAfterNextStop);
 
-	// Check that each frame is only advancing once
-	terminalSpinner.renderNextFrame();
-	expect(sizeAfterNextStop - sizeAfterStop).toEqual(
-		testWriter.buffer.length - sizeAfterNextStop
-	);
+  // Check that each frame is only advancing once
+  terminalSpinner.renderNextFrame();
+  expect(sizeAfterNextStop - sizeAfterStop).toEqual(
+    testWriter.buffer.length - sizeAfterNextStop,
+  );
 });
 
 Deno.test("check renderNextFrame can't be called if spinner is running", () => {
-	const terminalSpinner = new TerminalSpinner().start();
-	assertThrows(() => {
-		terminalSpinner.renderNextFrame();
-	}, Error);
-	terminalSpinner.stop();
+  const terminalSpinner = new TerminalSpinner().start();
+  assertThrows(() => {
+    terminalSpinner.renderNextFrame();
+  }, Error);
+  terminalSpinner.stop();
 });
 
 Deno.test("set() changes the terminalSpinner options", () => {
-	const testWriter = new TestWriter();
-	const SEARCH_KEY = "XXX";
+  const testWriter = new TestWriter();
+  const SEARCH_KEY = "XXX";
 
-	const terminalSpinner = new TerminalSpinner({
-		text: "sample",
-		writer: testWriter,
-	}).start();
+  const terminalSpinner = new TerminalSpinner({
+    text: "sample",
+    writer: testWriter,
+  }).start();
 
-	// Change the text to the search key and then check if it has actually changed
-	terminalSpinner.stopAndPersist();
-	terminalSpinner.set({ text: SEARCH_KEY });
-	terminalSpinner.renderNextFrame();
+  // Change the text to the search key and then check if it has actually changed
+  terminalSpinner.stopAndPersist();
+  terminalSpinner.set({ text: SEARCH_KEY });
+  terminalSpinner.renderNextFrame();
 
-	let inArray = false;
-	testWriter.buffer.forEach((item) => {
-		if (item.includes(SEARCH_KEY)) {
-			inArray = true;
-		}
-	});
+  let inArray = false;
+  testWriter.buffer.forEach((item) => {
+    if (item.includes(SEARCH_KEY)) {
+      inArray = true;
+    }
+  });
 
-	expect(inArray).toBe(true);
+  expect(inArray).toBe(true);
 });
--- usage-example-basic.ts
+++ usage-example-basic.ts
@@ -1,8 +1,10 @@
 import { TerminalSpinner } from "https://deno.land/x/spinners/mod.ts";
 import { sleep } from "https://x.nest.land/sleep@1.0.0/mod.ts";
 
-const terminalSpinner = new TerminalSpinner("I will be back in about 3 seconds");
+const terminalSpinner = new TerminalSpinner(
+  "I will be back in about 3 seconds",
+);
 
 terminalSpinner.start();
-await sleep(3) // or any other async action that'll take some time
+await sleep(3); // or any other async action that'll take some time
 terminalSpinner.succeed("Action completed");
